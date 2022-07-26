7569e714d6676614b0d99898121281fc0e1c6bb6
Saksham <sakshamsrini@gmail.com>
2022-07-26 17:42:00 +0530

    NFT blank error solved

--- src/components/TournamentView/common/checkNFTHolder.js
+++ src/components/TournamentView/common/checkNFTHolder.js
@@ -3,30 +3,28 @@ import { ethers } from "ethers";
 import NFTMarketPlace from "../../../contracts/NFTMarketplace.json";
 
 export default async function  checkNFTHolder(nft_list, account) {
-  // let providerWallet = await getCurrentWalletProvider();
-  // console.log(providerWallet);
-  let providerWallet = "https://rpc-mumbai.maticvigil.com";
-  // const signer = providerWallet.getSigner();
-  // "https://rpc-mumbai.maticvigil.com
-  const provider = new ethers.providers.JsonRpcProvider(providerWallet);
-  const contract = new ethers.Contract(
-    "0x635FA71793EB8d6f2f71D88560096805448a8E4C",
-    NFTMarketPlace.abi,
-    provider
-    // signer
-  );
-  console.log(nft_list);
-  console.log(account);
-  let returnValue = false;
-  for (let i=0; i< nft_list.length; i++){
-    console.log(nft_list[i].address);
-    if(await contract.checkNFTowner(
-        nft_list[i].address,
-        account
-    )){
-     returnValue = true;
-     break;
+  if(nft_list[0].name === '') {
+    let providerWallet = "https://polygon-mumbai.g.alchemy.com/v2/0fxK7G9YegGNIVwnav0XwkoMV9bb7qf1";
+    const provider = new ethers.providers.JsonRpcProvider(providerWallet);
+    const contract = new ethers.Contract(
+        "0x635FA71793EB8d6f2f71D88560096805448a8E4C",
+        NFTMarketPlace.abi,
+        provider
+    );
+    console.log(nft_list);
+    console.log(account);
+    let returnValue = false;
+    for (let i = 0; i < nft_list.length; i++) {
+      console.log(nft_list[i].address);
+      if (await contract.checkNFTowner(
+          nft_list[i].address,
+          account
+      )) {
+        returnValue = true;
+        break;
+      }
     }
+    return returnValue;
   }
-  return returnValue;
+  return false;
 }
