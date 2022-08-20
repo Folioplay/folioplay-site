4c85a9e291dcec4e3f7e08010a0acde03ecb569c
psyco_bot <66055014+ABHISHEK-WD-13@users.noreply.github.com>
2022-08-20 16:29:40 +0530

    minor changes in team preview
    Signed-off-by: Abhishek <aabhishek_be19@thapar.edu>

--- src/components/AssignRole/common/teamPreview.js
+++ src/components/AssignRole/common/teamPreview.js
@@ -1,52 +1,107 @@
 import { coinTosymbol } from "../../../CoinAndSymbols/symbols";
 export default function teamPreview({ superstars, mooning, rekt }) {
-    console.log(superstars);
-    for (var j = 0; j < superstars.length; j++) {
-        var bucketPreview = document.getElementsByClassName("superstars-preview");
-        if (superstars[j].selected) {
-            for (var i = 0; i < bucketPreview.length; i++) {
-                if (bucketPreview[i].childElementCount === 0) {
-                    var coinImage = document.createElement('img');
-                    coinImage.src = require('../../../images/coinLogos/' + coinTosymbol[superstars[j].name.toLowerCase()].toLowerCase() + ".png").default;
-                    coinImage.width = "80";
-                    coinImage.height = "80";
-                    coinImage.id = superstars[j].name + "-preview";
-                    bucketPreview[i].appendChild(coinImage);
-                    break;
-                }
-            }
+  console.log(superstars);
+  for (var j = 0; j < superstars.length; j++) {
+    var bucketPreview = document.getElementsByClassName("superstars-preview");
+    if (superstars[j].selected) {
+      var emptySpot = false;
+      for (var i = 0; i < bucketPreview.length; i++) {
+        if (bucketPreview[i].childElementCount === 0) {
+          var coinImage = document.createElement("img");
+          coinImage.src = require("../../../images/coinLogos/" +
+            coinTosymbol[superstars[j].name.toLowerCase()].toLowerCase() +
+            ".png").default;
+          coinImage.width = "80";
+          coinImage.height = "80";
+          coinImage.id = superstars[j].name + "-preview";
+          bucketPreview[i].appendChild(coinImage);
+          emptySpot = true;
+          break;
         }
+      }
+      if (!emptySpot) {
+        var newSpot = document.createElement("div");
+        var buckets = document.getElementsByClassName("bucket-preview");
+        newSpot.classList.add("superstars-preview", "coin-preview", "ml-20");
+        var coinImage = document.createElement("img");
+        coinImage.src = require("../../../images/coinLogos/" +
+          coinTosymbol[superstars[j].name.toLowerCase()].toLowerCase() +
+          ".png").default;
+        coinImage.width = "75";
+        coinImage.height = "75";
+        coinImage.id = superstars[j].name + "-preview";
+        newSpot.appendChild(coinImage);
+        newSpot.classList.add("mt-10");
+        buckets[0].appendChild(newSpot);
+      }
     }
-    for (var j = 0; j < mooning.length; j++) {
-        var bucketPreview = document.getElementsByClassName("mooning-preview");
-        if (mooning[j].selected) {
-            for (var i = 0; i < bucketPreview.length; i++) {
-                if (bucketPreview[i].childElementCount === 0) {
-                    var coinImage = document.createElement('img');
-                    coinImage.src = require('../../../images/coinLogos/' + coinTosymbol[mooning[j].name.toLowerCase()].toLowerCase() + ".png").default;
-                    coinImage.width = "80";
-                    coinImage.height = "80";
-                    coinImage.id = mooning[j].name + "-preview";
-                    bucketPreview[i].appendChild(coinImage);
-                    break;
-                }
-            }
+  }
+  for (var j = 0; j < mooning.length; j++) {
+    var bucketPreview = document.getElementsByClassName("mooning-preview");
+    if (mooning[j].selected) {
+      var emptySpot = false;
+      for (var i = 0; i < bucketPreview.length; i++) {
+        if (bucketPreview[i].childElementCount === 0) {
+          var coinImage = document.createElement("img");
+          coinImage.src = require("../../../images/coinLogos/" +
+            coinTosymbol[mooning[j].name.toLowerCase()].toLowerCase() +
+            ".png").default;
+          coinImage.width = "80";
+          coinImage.height = "80";
+          coinImage.id = mooning[j].name + "-preview";
+          bucketPreview[i].appendChild(coinImage);
+          emptySpot = true;
+          break;
         }
+      }
+      if (!emptySpot) {
+        var newSpot = document.createElement("div");
+        var buckets = document.getElementsByClassName("bucket-preview");
+        newSpot.classList.add("mooning-preview", "coin-preview", "ml-20");
+        var coinImage = document.createElement("img");
+        coinImage.src = require("../../../images/coinLogos/" +
+          coinTosymbol[mooning[j].name.toLowerCase()].toLowerCase() +
+          ".png").default;
+        coinImage.width = "75";
+        coinImage.height = "75";
+        coinImage.id = mooning[j].name + "-preview";
+        newSpot.appendChild(coinImage);
+        buckets[1].appendChild(newSpot);
+      }
     }
-    for (var j = 0; j < rekt.length; j++) {
-        var bucketPreview = document.getElementsByClassName("rekt-preview");
-        if (rekt[j].selected) {
-            for (var i = 0; i < bucketPreview.length; i++) {
-                if (bucketPreview[i].childElementCount === 0) {
-                    var coinImage = document.createElement('img');
-                    coinImage.src = require('../../../images/coinLogos/' + coinTosymbol[rekt[j].name.toLowerCase()].toLowerCase() + ".png").default;
-                    coinImage.width = "80";
-                    coinImage.height = "80";
-                    coinImage.id = rekt[j].name + "-preview";
-                    bucketPreview[i].appendChild(coinImage);
-                    break;
-                }
-            }
+  }
+  for (var j = 0; j < rekt.length; j++) {
+    var bucketPreview = document.getElementsByClassName("rekt-preview");
+    if (rekt[j].selected) {
+      var emptySpot = false;
+      for (var i = 0; i < bucketPreview.length; i++) {
+        if (bucketPreview[i].childElementCount === 0) {
+          var coinImage = document.createElement("img");
+          coinImage.src = require("../../../images/coinLogos/" +
+            coinTosymbol[rekt[j].name.toLowerCase()].toLowerCase() +
+            ".png").default;
+          coinImage.width = "80";
+          coinImage.height = "80";
+          coinImage.id = rekt[j].name + "-preview";
+          bucketPreview[i].appendChild(coinImage);
+          emptySpot = true;
+          break;
         }
+      }
+      if (!emptySpot) {
+        var newSpot = document.createElement("div");
+        var buckets = document.getElementsByClassName("bucket-preview");
+        newSpot.classList.add("rekt-preview", "coin-preview", "ml-20");
+        var coinImage = document.createElement("img");
+        coinImage.src = require("../../../images/coinLogos/" +
+          coinTosymbol[rekt[j].name.toLowerCase()].toLowerCase() +
+          ".png").default;
+        coinImage.width = "75";
+        coinImage.height = "75";
+        coinImage.id = rekt[j].name + "-preview";
+        newSpot.appendChild(coinImage);
+        buckets[2].appendChild(newSpot);
+      }
     }
-} 
+  }
+}
--- src/components/TeamCreation/common/TeamPreview.js
+++ src/components/TeamCreation/common/TeamPreview.js
@@ -1,48 +1,50 @@
 import React from "react";
 
-export default function TeamPreview({ }) {
-    // superstars = [{ "name": "Bitcoin", "symbol": "btc", "isSelected": true }, { "name": "Ethereum", "symbol": "eth", "isSelected": true }];
-    // mooning = [{ "name": "Bitcoin", "symbol": "btc", "isSelected": true }, { "name": "Ethereum", "symbol": "eth", "isSelected": true }];
-    // rekt = [{ "name": "Bitcoin", "symbol": "btc", "isSelected": true }, { "name": "Ethereum", "symbol": "eth", "isSelected": true }];
-    var superstars = [{ name: "default" }]
-    var mooning = [{ name: "default" }, { name: "default" }, { name: "default" }]
-    var rekt = [{ name: "default" }, { name: "default" }, { name: "default" }]
-    return (
-        <div className="team-preview">
-            <div className="bucket-preview mb-20">
-                {/* <span className="font-size-20 mr-20">Superstar</span> */}
-                {superstars.map((coin, index) => {
-                    return (
-                        <div className="superstars-preview coin-preview ml-20 mt-10">
-                            {/* <img src={require('../../../images/coinLogos/' + coin.symbol.toLowerCase() + '.png').default} width="80" height="80" /> */}
-                        </div>
-
-                    );
-                })}
+export default function TeamPreview({}) {
+  // superstars = [{ "name": "Bitcoin", "symbol": "btc", "isSelected": true }, { "name": "Ethereum", "symbol": "eth", "isSelected": true }];
+  // mooning = [{ "name": "Bitcoin", "symbol": "btc", "isSelected": true }, { "name": "Ethereum", "symbol": "eth", "isSelected": true }];
+  // rekt = [{ "name": "Bitcoin", "symbol": "btc", "isSelected": true }, { "name": "Ethereum", "symbol": "eth", "isSelected": true }];
+  var superstars = [{ name: "default" }];
+  var mooning = [{ name: "default" }, { name: "default" }, { name: "default" }];
+  var rekt = [{ name: "default" }, { name: "default" }, { name: "default" }];
+  return (
+    <div className="team-preview">
+      <div className="bucket-preview mb-20">
+        <span className="font-size-20 mr-20" style={{ display: "inline" }}>
+          Superstar
+        </span>
+        {superstars.map((coin, index) => {
+          return (
+            <div className="superstars-preview coin-preview ml-20 mt-10">
+              {/* <img src={require('../../../images/coinLogos/' + coin.symbol.toLowerCase() + '.png').default} width="80" height="80" /> */}
             </div>
-            <div className="bucket-preview mb-20 mt-10">
-                {/* <span className="font-size-20 mr-20">Mooning</span> */}
-                {mooning.map((coin, index) => {
-                    return (
-                        <div className="mooning-preview coin-preview ml-20">
-                            {/* <img src={require('../../../images/coinLogos/' + coin.symbol.toLowerCase() + '.png').default} width="80" height="80" /> */}
-                        </div>
-
-                    );
-
-                })}
+          );
+        })}
+      </div>
+      <div className="bucket-preview mb-20 mt-10">
+        <span className="font-size-20 mr-20" style={{ display: "inline" }}>
+          Mooning
+        </span>
+        {mooning.map((coin, index) => {
+          return (
+            <div className="mooning-preview coin-preview ml-20">
+              {/* <img src={require('../../../images/coinLogos/' + coin.symbol.toLowerCase() + '.png').default} width="80" height="80" /> */}
             </div>
-            <div className="bucket-preview mb-20 mt-10">
-                {/* <span className="font-size-20 mr-20">Rekt</span> */}
-                {rekt.map((coin, index) => {
-                    return (
-                        <div className="rekt-preview coin-preview ml-20">
-                            {/* <img src={require('../../../images/coinLogos/' + coin.symbol.toLowerCase() + '.png').default} width="80" height="80" /> */}
-                        </div>
-
-                    );
-                })}
+          );
+        })}
+      </div>
+      <div className="bucket-preview mb-20 mt-10">
+        <span className="font-size-20 mr-20" style={{ display: "inline" }}>
+          Rekt
+        </span>
+        {rekt.map((coin, index) => {
+          return (
+            <div className="rekt-preview coin-preview ml-20">
+              {/* <img src={require('../../../images/coinLogos/' + coin.symbol.toLowerCase() + '.png').default} width="80" height="80" /> */}
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
