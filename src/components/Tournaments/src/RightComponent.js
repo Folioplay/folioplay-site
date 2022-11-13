5e6c58d219c385e27cb643138630947dd5fce9f7
Saksham <sakshamsrini@gmail.com>
2022-11-14 05:02:43 +0530

    build updated

--- src/components/Tournaments/src/RightComponent.js
+++ src/components/Tournaments/src/RightComponent.js
@@ -24,105 +24,105 @@ const style = {
 };
 
 const RightComponent = () => {
-  const dispatch = useDispatch();
-  const [snackOpen, setSnackOpen] = React.useState(false);
-  const openReferralRedux = useSelector((state)=> state.LeaderBoardSlice.referralModal)
-
-  const snackHandleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setSnackOpen(false);
-  };
-
-  const [snackMessage, setSnackMessage] = useState("");
-  const [snackSeverityType, setSnackSeverityType] = useState("");
-
-  const [open, setOpen] = useState(
-    localStorage.getItem("user_referral") === "null"
-  );
-  const handleClose = () => {
-    dispatch(closeReferralModal());
-    // setOpen(false);
-  };
-  const handleOpen = () => {
-    dispatch(openReferralModal);
-    // setOpen(true);
-  }
-  const referralExists = localStorage.getItem("user_referral");
-
-  const ReferralModal = () => {
-    const [referral, setReferral] = useState("");
-    // useEffect(() => {
-    //   if (referralExists !== "null") {
-    //     setReferral(localStorage.getItem("user_referral"));
-    //   }
-    // },[]);
-
-    return (
-        <Modal
-            open={openReferralRedux}
-            onClose={handleClose}
-            className="modal"
-            style={{whiteSpace: 'pre-line'}}
-        >
-        <Box sx={style}>
-          <div className="referralModal__content">
-            <div className="referralModal__modalHeading">
-              <Typography
-                className="referralHeader"
-                id="modal-modal-title"
-                variant="h6"
-                component="h2"
-              >
-                Enter the referral code below
-              </Typography>
-            </div>
-            <div className="referralModal__inputBox">
-              <TextField
-                className="referralModal__modalText"
-                id="outlined-basic"
-                label="Referral Code"
-                variant="outlined"
-                onChange={(e) => setReferral(e.target.value)}
-              />
-              <Button
-                className="referralModal__modalButton"
-                variant={"contained"}
-                onClick={async () => {
-                  const referralResponse = await referralCodePost(referral);
-                  if (referralResponse.statusCode === 200) {
-                    setSnackMessage("Referral successful");
-                    setSnackSeverityType("success");
-                  } else {
-                    if (referralResponse.statusCode === 400) {
-                      setSnackMessage(referralResponse.message);
-                      setSnackSeverityType("error");
-                    }
-                  }
-                  setSnackOpen(true);
-                  dispatch(closeReferralModal());
-                }}
-              >
-                Let's Play
-              </Button>
-            </div>
-          </div>
-        </Box>
-        </Modal>
-    );
-  };
+  // const dispatch = useDispatch();
+  // const [snackOpen, setSnackOpen] = React.useState(false);
+  // const openReferralRedux = useSelector((state)=> state.LeaderBoardSlice.referralModal)
+  //
+  // const snackHandleClose = (event, reason) => {
+  //   if (reason === "clickaway") {
+  //     return;
+  //   }
+  //   setSnackOpen(false);
+  // };
+  //
+  // const [snackMessage, setSnackMessage] = useState("");
+  // const [snackSeverityType, setSnackSeverityType] = useState("");
+  //
+  // const [open, setOpen] = useState(
+  //   localStorage.getItem("user_referral") === "null"
+  // );
+  // const handleClose = () => {
+  //   dispatch(closeReferralModal());
+  //   // setOpen(false);
+  // };
+  // const handleOpen = () => {
+  //   dispatch(openReferralModal);
+  //   // setOpen(true);
+  // }
+  // const referralExists = localStorage.getItem("user_referral");
+  //
+  // const ReferralModal = () => {
+  //   const [referral, setReferral] = useState("");
+  //   // useEffect(() => {
+  //   //   if (referralExists !== "null") {
+  //   //     setReferral(localStorage.getItem("user_referral"));
+  //   //   }
+  //   // },[]);
+  //
+  //   return (
+  //       <Modal
+  //           open={openReferralRedux}
+  //           onClose={handleClose}
+  //           className="modal"
+  //           style={{whiteSpace: 'pre-line'}}
+  //       >
+  //       <Box sx={style}>
+  //         <div className="referralModal__content">
+  //           <div className="referralModal__modalHeading">
+  //             <Typography
+  //               className="referralHeader"
+  //               id="modal-modal-title"
+  //               variant="h6"
+  //               component="h2"
+  //             >
+  //               Enter the referral code below
+  //             </Typography>
+  //           </div>
+  //           <div className="referralModal__inputBox">
+  //             <TextField
+  //               className="referralModal__modalText"
+  //               id="outlined-basic"
+  //               label="Referral Code"
+  //               variant="outlined"
+  //               onChange={(e) => setReferral(e.target.value)}
+  //             />
+  //             <Button
+  //               className="referralModal__modalButton"
+  //               variant={"contained"}
+  //               onClick={async () => {
+  //                 const referralResponse = await referralCodePost(referral);
+  //                 if (referralResponse.statusCode === 200) {
+  //                   setSnackMessage("Referral successful");
+  //                   setSnackSeverityType("success");
+  //                 } else {
+  //                   if (referralResponse.statusCode === 400) {
+  //                     setSnackMessage(referralResponse.message);
+  //                     setSnackSeverityType("error");
+  //                   }
+  //                 }
+  //                 setSnackOpen(true);
+  //                 dispatch(closeReferralModal());
+  //               }}
+  //             >
+  //               Let's Play
+  //             </Button>
+  //           </div>
+  //         </div>
+  //       </Box>
+  //       </Modal>
+  //   );
+  // };
 
   return (
     <div id="tournament-page-image">
       {/* {referralExists !== "null" ? null : <ReferralModal />} */}
-      <ReferralModal />
-      <SnackbarComponent
-        open={snackOpen}
-        handleClose={snackHandleClose}
-        message={snackMessage}
-        severityType={snackSeverityType}
-      />
+      {/*<ReferralModal />*/}
+      {/*<SnackbarComponent*/}
+      {/*  open={snackOpen}*/}
+      {/*  handleClose={snackHandleClose}*/}
+      {/*  message={snackMessage}*/}
+      {/*  severityType={snackSeverityType}*/}
+      {/*/>*/}
       <span className="font-size-36 font-weight-700 mb-0">
         Let the game begin!
       </span>
