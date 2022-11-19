52ed0d9b28311df08c23c00fbf7ff415308207c0
Saksham <sakshamsrini@gmail.com>
2022-11-19 17:16:12 +0530

    build conflict resolved

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
- import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,8 +7,6 @@ import { MoralisProvider } from "react-moralis";
 import {AuthContextProvider} from "./Context/AuthContext";
 import {Provider as ReduxProvider} from "react-redux";
 import store from "./Redux/Store";
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
