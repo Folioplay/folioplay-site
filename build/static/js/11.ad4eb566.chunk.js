07bbab3a0777fe0fff107470b2edd2f51a850e03
ABHISHEK-WD-13 <aabhishek_be19@thapar.edu>
2022-11-20 18:42:20 +0530

    build

---
+++ build/static/js/11.ad4eb566.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfolioplay=this.webpackJsonpfolioplay||[]).push([[11,10,12],{1705:function(o,n){},1712:function(o,n){},1714:function(o,n){}}]);
+//# sourceMappingURL=11.ad4eb566.chunk.js.map
