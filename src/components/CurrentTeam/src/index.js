4ac0081d88c1a4f82e0fa4d2a42631c97b59e4c6
ABHISHEK-WD-13 <aabhishek_be19@thapar.edu>
2022-11-21 13:20:51 +0530

    removed toFixed

--- src/components/CurrentTeam/src/index.js
+++ src/components/CurrentTeam/src/index.js
@@ -91,7 +91,7 @@ export default function CurrentTeamPreview() {
                           className="font-size-12"
                           style={{ color: "var(--dark-dim-white)" }}
                         >
-                          Points {coin.current_points.toFixed(2)}
+                          Points {coin.current_points}
                         </span>
                       </motion.div>
                     </Grid>
