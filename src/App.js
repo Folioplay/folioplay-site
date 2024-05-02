5cddc6eb8497889a024ac17d25c37641e70c93e7
Naman <namanj413@gmail.com>
2024-05-02 22:06:57 +0530

    temprory
    temprory

--- src/App.js
+++ src/App.js
@@ -300,7 +300,6 @@ import ReactGA from "react-ga4";
 import SelectedCoinTeamPreview from "./components/SelectedCoinTeamPreview/src";
 import HowToPlay from "./components/HowToPlay/src";
 import PrivacyPolicyPage from "./components/PrivacyPolicyPage/src";
-import Feedback from "./components/Feedback/src";
 
 
 
@@ -509,16 +508,6 @@ const [token, setToken] =useState("");
             element={
               <AuthenticatedRoute>
                   <HowToPlay />
-
-              </AuthenticatedRoute>
-            }
-          />
-          <Route
-            exact
-            path="/feedback"
-            element={
-              <AuthenticatedRoute>
-                  <Feedback />
 
               </AuthenticatedRoute>
             }
--- src/components/LoginPage/src/LoginVerify.js
+++ src/components/LoginPage/src/LoginVerify.js
@@ -1,23 +1,21 @@
 import { useEffect, useState } from "react";
 // import { magic } from "../lib/magic";
 import { json, useNavigate } from "react-router-dom";
-import { Magic } from 'magic-sdk';
+import { Magic } from "magic-sdk";
 import { OAuthExtension } from "@magic-ext/oauth";
 import { getMagicAuthToken } from "../../../APIS/apis";
 import ReactLoading from "react-loading";
-import CircularProgress from '@mui/material/CircularProgress';
-import LinearProgress from '@mui/material/LinearProgress';
-import Box from '@mui/material/Box';
-import folioPlayLogo from "../../../images/FPLogoManifest.png"
+import CircularProgress from "@mui/material/CircularProgress";
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
+import folioPlayLogo from "../../../images/FPLogoManifest.png";
 
-import folioPlayLogoText from "../../../images/white_folioplay.svg"
+import folioPlayLogoText from "../../../images/white_folioplay.svg";
 const LoginVerify = () => {
-  const [res, setRes] = useState(' ');
+  const [res, setRes] = useState(" ");
   const [user, setUser] = useState("");
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
-
-
 
   // const magic = new Magic("pk_live_3CE84BD39270E181", {
   //   extensions: [new OAuthExtension()],
@@ -53,64 +51,56 @@ const LoginVerify = () => {
   const finishSocialLogin = async () => {
     setIsLoading(true);
     const Loggedinn = await localStorage.getItem("isLoggedIn");
-    if (!Loggedinn) {
+    if (Loggedinn) {
       setIsLoading(true);
       try {
-        const result = await magic.oauth.getRedirectResult();
+        // const result = await magic.oauth.getRedirectResult();
 
-        await setUser(result);
+        // await setUser(result);
         // await console.log("result neeche hai");
         //   // setRes(result);
         //   await console.log("json uper hai");
         //   await console.log(user);
         //   await console.log("result uper hai");
 
-        const meta = await magic.user.getMetadata();
+        // const meta = await magic.user.getMetadata();
         // console.log("meta data 31 line"+ JSON.stringify(meta))
         //  console.log("string resposnse  user data 32 line"+" "+JSON.stringify(user))
         // console.log("user resposnse 33 line"+ user);
-        const isLoggedIn = await magic.user.isLoggedIn();
-        localStorage.setItem("isLoggedIn", isLoggedIn);
-        const didToken = await result.magic.idToken;
-        await localStorage.setItem("didToken", didToken);
+        // const isLoggedIn = await magic.user.isLoggedIn();
+        // localStorage.setItem("isLoggedIn", isLoggedIn);
+        const didToken = localStorage.getItem("didToken");
+        // await localStorage.setItem("didToken", didToken);
 
         //  await  localStorage.setItem("didToken", didToken);
-        const userEmail = await result.magic.userMetadata.email;
-        await localStorage.setItem("user", userEmail);
-        const walletAddress = await result.magic.userMetadata.publicAddress;
-        await localStorage.setItem("walletAddress", walletAddress);
-        await getMagicAuthToken(didToken, walletAddress);
+        // const userEmail = await result.magic.userMetadata.email;
+        // await localStorage.setItem("user", userEmail);
+        const walletAddress = localStorage.getItem("walletAddress");
+        // await localStorage.setItem("walletAddress", walletAddress);
+        const userdata = await getMagicAuthToken(didToken, walletAddress);
 
-
-        navigate("/tournaments");
-        window.location.reload();
-
+        // navigate("/tournaments");
+        // window.location.reload();
       } catch (err) {
         console.error(err);
       }
-
     } else {
-      // navigate("/tournaments")
+      // navigate("/")
     }
-
-  }
-
-
+  };
 
   useEffect(() => {
-
     setIsLoading(true);
     const isLoggedIn = localStorage.getItem("isLoggedIn");
 
     if (isLoggedIn === "true") {
-    setTimeout(() => {
-      navigate("/tournaments");
-      window.location.reload();
-    }, 2000);
-    } else {
       finishSocialLogin();
+    } else {
+      setTimeout(() => {
+        navigate("/");
+        window.location.reload();
+      }, 2000);
     }
-
   }, []);
 
   const logout = async () => {
@@ -125,7 +115,6 @@ const LoginVerify = () => {
   const cons = async () => {
     try {
       //  console.log(user)
-
       // console.log(user.magic.userMetadata.email)
     } catch (err) {
       console.error(err);
@@ -141,34 +130,58 @@ const LoginVerify = () => {
         <div  className="bli">
 <img src={folioPlayLogo} alt="folioplayLogo" style={{zIndex:"1",width:"50%"}} />
         </div></div> </div> */}
-        <Box sx={{ width: '100vw', height: "100vh", backgroundColor: "#453df1", position: "absolute" }}>
-
-          <LinearProgress style={{ color: "blue", padding: "2px 0 0 0", backgroundColor: "white" }} />
+        <Box
+          sx={{
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "#453df1",
+            position: "absolute",
+          }}
+        >
+          <LinearProgress
+            style={{
+              color: "blue",
+              padding: "2px 0 0 0",
+              backgroundColor: "white",
+            }}
+          />
           <br />
-          <Box sx={{ display: 'flex', justifyContent: "center", width: "100%", backgroundColor: "blue" }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              width: "100%",
+              backgroundColor: "blue",
+            }}
+          >
             <div className="loginverfiyPageBlink">
               <div className="blinkData">
                 <div>
-                  <img src={folioPlayLogo} alt="folioplayLogo" className="loginVerifyFPLogo" style={{ zIndex: "1" }} />
+                  <img
+                    src={folioPlayLogo}
+                    alt="folioplayLogo"
+                    className="loginVerifyFPLogo"
+                    style={{ zIndex: "1" }}
+                  />
                 </div>
-                <div> <img src={folioPlayLogoText} alt="folioplayLogo" className="loginVerifyFPText" style={{ zIndex: "1" }} />
+                <div>
+                  {" "}
+                  <img
+                    src={folioPlayLogoText}
+                    alt="folioplayLogo"
+                    className="loginVerifyFPText"
+                    style={{ zIndex: "1" }}
+                  />
                 </div>
-              </div></div>
+              </div>
+            </div>
           </Box>
-
         </Box>
       </>
-    )
-  } else {
-    return (
-      <>
-        Loading...
-      </>
     );
+  } else {
+    return <>Loading...</>;
   }
-
-
 };
 
 export default LoginVerify;
-
