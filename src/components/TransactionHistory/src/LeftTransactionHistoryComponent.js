6cfb27d812abcc89848c5851b1058545eaf84caa
Naman <namanj413@gmail.com>
2024-06-04 21:23:58 +0530

    fixed
    fixed

--- src/components/TransactionHistory/src/LeftTransactionHistoryComponent.js
+++ src/components/TransactionHistory/src/LeftTransactionHistoryComponent.js
@@ -1,176 +1,215 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import FolioplayBar from "../../FolioplayBar/src";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import { Link, useNavigate ,useLocation} from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { getTransactionHistory } from "../../../APIS/apis";
 import moment from "moment";
 import AccordionComponent from "../../../Common/Accordion";
 import { AccordionDetails, AccordionSummary } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Typography from "@mui/material/Typography";
-import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion from '@mui/material/Accordion';
-import MuiAccordionSummary from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
-import { SERVER } from '../../../APIS/apis';
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import { SERVER } from "../../../APIS/apis";
 
 const LeftTransactionHistoryComponent = () => {
-    const { state } = useLocation();
-    const AccordionSummary = styled((props) => (
-        <MuiAccordionSummary
-            expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
-            {...props}
-        />
-    ))(({ theme }) => ({
-        // backgroundColor:
-        //     theme.palette.mode === 'dark'
-        //         ? 'rgba(255, 255, 255, .05)'
-        //         : 'rgba(0, 0, 0, .03)',
-    }));
+  const { state } = useLocation();
+  const AccordionSummary = styled((props) => (
+    <MuiAccordionSummary
+      expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    // backgroundColor:
+    //     theme.palette.mode === 'dark'
+    //         ? 'rgba(255, 255, 255, .05)'
+    //         : 'rgba(0, 0, 0, .03)',
+  }));
 
-    const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-        padding: theme.spacing(2),
-        borderTop: '1px solid rgba(0, 0, 0, .125)',
-    }));
+  const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(2),
+    borderTop: "1px solid rgba(0, 0, 0, .125)",
+  }));
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [transactionHistory, setTransactionHistory] = useState([]);
+  const [transactionHistory, setTransactionHistory] = useState([]);
 
-    function parseTransactionData(data) {
-        let parsedData = {};
-        for (let i = 0; i < data.length; i++) {
-            let dateKey = moment(data[i].date).format('Do MMMM YYYY');
-            if (!parsedData.hasOwnProperty(`${dateKey}`)) {
-                parsedData[`${dateKey}`] = [data[i]];
-            }
-            else {
-                parsedData[`${dateKey}`].push(data[i]);
-            }
-        }
-        const keyArray = Object.keys(parsedData);
-        let arr = []
-        for (let i = 0; i < keyArray.length; i++) {
-            arr.push({
-                "date": keyArray[i],
-                "data": parsedData[keyArray[i]]
-            })
-        }
-        setTransactionHistory(arr);
-    }
-
-    const NavigateTournament = (TournamentId) => {
-
-
-        console.log("TournamentId" + TournamentId)
-    }
-
-    useEffect(() => {
-        async function setTransactionHistoryFunction() {
-            const data = await getTransactionHistory();
-            parseTransactionData(data);
-            console.log(data);
-        }
-        setTransactionHistoryFunction();
-    }, [])
-
-    async function checkState(){
-        if(state && state.comingFrom == "/tournament") {
-           navigate("/tournaments" , {state:{comingFrom:""}});
-         }else{
-            navigate(-1);
-         }
+  function parseTransactionData(data) {
+    let parsedData = {};
+    for (let i = 0; i < data.length; i++) {
+      let dateKey = moment(data[i].date).format("Do MMMM YYYY");
+      if (!parsedData.hasOwnProperty(`${dateKey}`)) {
+        parsedData[`${dateKey}`] = [data[i]];
+      } else {
+        parsedData[`${dateKey}`].push(data[i]);
       }
-    const localAccordionComponent = (item) => {
-        return (
-            <AccordionComponent>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
-                    <div className="dateBlock__accordionList__summaryTab">
-                      
-                        {parseFloat(item.data.amount) ?  (<div className="summaryTab__amount">{item.type === "PAID" ? "-" : "+"}{item.data.amount} FPC </div>):(<div className="summaryTab__amount">{item.type === "PAID" ? "-" : "+"}{item.data.amount} </div>)}
-                       
-                        <div className={"summaryTab__helperText"} style={{ marginLeft: "20px", marginRight: "auto" }}>{item.type === "RECEIVED" ? "Reward" : "Joined Tournament"}</div>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <div className={"summaryTab__extendedDetails"}>
-                        <div className={"summaryTab__dataHeading"}>
-                            Tournament Name
-                        </div>
-                        <div className={"summaryTab__dataBody"} style={{ cursor:"pointer",color:"blue",textDecoration:"underline"}} onClick={() => {
-                            navigate(`/tournament/${item.data.fromId}`, {
-                                state: {
-                                    comingFrom: window.location.pathname,
-                                },
-                            })
-                        }
-                        }>
-                            {item.data.tournamentName}
-                        </div>
-                        <div className={"summaryTab__dataHeading"}>
-                            Transaction ID
-                        </div>
-                        <div className={"summaryTab__dataBody"}>
-                            {item._id}
-                        </div>
-                        <div className={"summaryTab__dataHeading"}>
-                            Transaction Date
-                        </div>
-                        <div className={"summaryTab__dataBody"}>
-                            {moment(item.date).format('Do MMMM YYYY, h:mm:ss a')}
-                        </div>
-                        <div className={"summaryTab__dataHeading"}>
-                            Team Name
-                        </div>
-                        <div className={"summaryTab__dataBody"}>
-                            {item.data.teamName}
-                        </div>
-                    </div>
-                </AccordionDetails>
-            </AccordionComponent>
-        )
     }
+    const keyArray = Object.keys(parsedData);
+    let arr = [];
+    for (let i = 0; i < keyArray.length; i++) {
+      arr.push({
+        date: keyArray[i],
+        data: parsedData[keyArray[i]],
+      });
+    }
+    setTransactionHistory(arr);
+  }
 
+  const NavigateTournament = (TournamentId) => {
+    console.log("TournamentId" + TournamentId);
+  };
+
+  useEffect(() => {
+    async function setTransactionHistoryFunction() {
+      const data = await getTransactionHistory();
+      parseTransactionData(data);
+      console.log(data);
+    }
+    setTransactionHistoryFunction();
+  }, []);
+
+  async function checkState() {
+    if (state && state.comingFrom == "/tournament") {
+      navigate("/tournaments", { state: { comingFrom: "" } });
+    } else {
+      navigate(-1);
+    }
+  }
+  const localAccordionComponent = (item) => {
     return (
-        <div className="transactionHistory__fullPage">
-            <div className="transactionHistory__header">
-                <ArrowBackIosIcon
-                    fontSize="medium"
-                    className="go-back-button"
-                    onClick={() => checkState()}
-                />
-                <span className="ml-20 font-size-20 font-weight-700">
-                    {"Transaction History"}
-                </span>
+      <AccordionComponent>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <div className="dateBlock__accordionList__summaryTab">
+            {parseFloat(
+              item.type === "WITHDRAWAL_REJECTED" ? 0 : item.data.amount
+            ) ? (
+              <div className="summaryTab__amount">
+                {item.type === "PAID" ||
+                "WITHDRAWAL" ||
+                "WITHDRAWAL_REJECTED" ||
+                "WITHDRAWAL"
+                  ? "-"
+                  : "+"}
+                {item.type === "WITHDRAWAL_REJECTED" ? 0 : item.data.amount} FPC{" "}
+              </div>
+            ) : (
+              <div className="summaryTab__amount">
+                {item.type === "PAID" ? "-" : "+"}
+                {item.type === "WITHDRAWAL_REJECTED"
+                  ? 0
+                  : item.data.amount}{" "}
+              </div>
+            )}
+
+            <div
+              className={"summaryTab__helperText"}
+              style={{ marginLeft: "20px", marginRight: "auto" }}
+            >
+              {item.type === "RECEIVED" && "Reward"}
+              {item.type === "RECEIVED" && "Reward"}
+              {item.type === "WITHDRAWAL_REJECTED" && "WITHDRAWAL REJECTED"}
+              {item.type === "WITHDRAWAL" && "WITHDRAWAL"}
             </div>
-            <div className="transactionHistory__body">
-                {transactionHistory.length !== 0 ?
-                    transactionHistory.map((data, index) => {
-                        return (
-                            <div className="transactionHistory__dateBlock">
-                                <div className="dateBlock__date">
-                                    {data.date}
-                                </div>
-                                <div className="dateBlock__transactionAccordion">
-                                    {data.data.map((item, innerIndex) => {
-                                        return (
-                                            <div className={"dateBlock__accordionList"}>
-                                                {localAccordionComponent(item)}
-                                            </div>
-                                        )
-                                    })}
-                                </div>
-                            </div>
-                        )
-                    }) : <h3>No transactions yet.</h3>}
+          </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div className={"summaryTab__extendedDetails"}>
+            <div className={"summaryTab__dataHeading"}>
+              {item.type === "WITHDRAWAL_REJECTED" && "WITHDRAWAL REJECTED"}
+              {item.type === "WITHDRAWAL" && "WITHDRAWAL"}
+              {item.type === "TOURNAMENT" && " Tournament Name"}
             </div>
-        </div>
+
+            {item.type === "WITHDRAWAL_REJECTED" || "WITHDRAWAL" ? (
+              ""
+            ) : (
+              <>
+                <div
+                  className={"summaryTab__dataBody"}
+                  style={{
+                    cursor: "pointer",
+                    color: "blue",
+                    textDecoration: "underline",
+                  }}
+                  onClick={() => {
+                    navigate(`/tournament/${item.data.fromId}`, {
+                      state: {
+                        comingFrom: window.location.pathname,
+                      },
+                    });
+                  }}
+                >
+                  {item.type === "WITHDRAWAL_REJECTED" || "WITHDRAWAL"
+                    ? 0
+                    : item.data.tournamentName}
+                </div>
+              </>
+            )}
+
+            <div className={"summaryTab__dataHeading"}>Transaction ID</div>
+            <div className={"summaryTab__dataBody"}>{item._id}</div>
+            <div className={"summaryTab__dataHeading"}>Transaction Date</div>
+            <div className={"summaryTab__dataBody"}>
+              {moment(item.date).format("Do MMMM YYYY, h:mm:ss a")}
+            </div>
+            <div className={"summaryTab__dataHeading"}>  {item.type === "WITHDRAWAL_REJECTED" || "WITHDRAWAL"
+                ? ""
+                : "Team Name"}</div>
+            <div className={"summaryTab__dataBody"}>
+              {item.type === "WITHDRAWAL_REJECTED" || "WITHDRAWAL"
+                ? ""
+                : item.data.teamName}
+            </div>
+          </div>
+        </AccordionDetails>
+      </AccordionComponent>
     );
+  };
+
+  return (
+    <div className="transactionHistory__fullPage">
+      <div className="transactionHistory__header">
+        <ArrowBackIosIcon
+          fontSize="medium"
+          className="go-back-button"
+          onClick={() => checkState()}
+        />
+        <span className="ml-20 font-size-20 font-weight-700">
+          {"Transaction History"}
+        </span>
+      </div>
+      <div className="transactionHistory__body">
+        {transactionHistory.length !== 0 ? (
+          transactionHistory.map((data, index) => {
+            return (
+              <div className="transactionHistory__dateBlock">
+                <div className="dateBlock__date">{data.date}</div>
+                <div className="dateBlock__transactionAccordion">
+                  {data.data.map((item, innerIndex) => {
+                    return (
+                      <div className={"dateBlock__accordionList"}>
+                        {localAccordionComponent(item)}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <h3>No transactions yet.</h3>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default LeftTransactionHistoryComponent;
